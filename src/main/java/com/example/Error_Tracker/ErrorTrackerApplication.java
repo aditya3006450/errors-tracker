8f85f95559a7aaa75fb3c60b22a31935ca3ba467
aditya3006450 <ankyy5307@gmail.com>
2025-11-26 11:22:23 +0530

    app initialization

---
+++ src/main/java/com/example/Error_Tracker/ErrorTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.Error_Tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrorTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErrorTrackerApplication.class, args);
+	}
+
+}
